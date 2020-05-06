4d657973f3649fc573293c10804c89c8af45cbd7
andremlsantos <andremlsantos@gmail.com>
2020-05-06 18:10:59 +0200

    Add && Delete Contact
    Added 2 new actions
    Added 2 new types
    Contact Reducer for Add and Delete

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
 import { connect } from "react-redux";
+import { getContacts } from "../../actions/contactActions";
 import PropTypes from "prop-types";
-import { GET_CONTACTS } from "../../actions/types";
 
 class Contacts extends Component {
     componentDidMount() {
@@ -34,8 +34,4 @@ const mapStateToProps = (state) => ({
     contacts: state.contact.contacts,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    getContacts: () => dispatch({ type: GET_CONTACTS }),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
+export default connect(mapStateToProps, { getContacts })(Contacts);
