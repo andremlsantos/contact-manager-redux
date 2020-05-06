e34639a293fd2255491f9ccae86cac4c8b752692
andremlsantos <andremlsantos@gmail.com>
2020-05-06 19:51:21 +0200

    Updates

---
+++ precache-manifest.36d0a3be1282bbd2210b8baccc4516a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108883c7b234cd27228313a47c3bcd04",
+    "url": "/contact-manager-redux/index.html"
+  },
+  {
+    "revision": "1081671f8fd935005447",
+    "url": "/contact-manager-redux/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c32145270263eeb7906d",
+    "url": "/contact-manager-redux/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1081671f8fd935005447",
+    "url": "/contact-manager-redux/static/js/2.033beea0.chunk.js"
+  },
+  {
+    "revision": "1256f8b03db390ff016631fa1aea565e",
+    "url": "/contact-manager-redux/static/js/2.033beea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32145270263eeb7906d",
+    "url": "/contact-manager-redux/static/js/main.6dc11359.chunk.js"
+  },
+  {
+    "revision": "9cb60d772239c2ad278e",
+    "url": "/contact-manager-redux/static/js/runtime-main.8a81168d.js"
+  }
+]);
